c4bbaad50c573d1db9f8ba1c8b45a44875c4af1e
chinedu moses <chinedu2moses@gmail.com>
2020-06-24 13:45:39 -0500

    Updates

---
+++ precache-manifest.a34e3df0e093d5a090f8fc5731be2239.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22490d247bfb01cae881329e97c1e2d6",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f36e232cde913a3350dc",
+    "url": "/clicky-game/static/css/main.c32661e1.chunk.css"
+  },
+  {
+    "revision": "d00f6fb00b85b6db64aa",
+    "url": "/clicky-game/static/js/2.2ff7c561.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.2ff7c561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f36e232cde913a3350dc",
+    "url": "/clicky-game/static/js/main.a768389b.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  },
+  {
+    "revision": "fd6a33c36d19e6da8972e7c7d10ba4ec",
+    "url": "/clicky-game/static/media/eyes1.fd6a33c3.png"
+  },
+  {
+    "revision": "5bb565e5a7b1d734ce1049ef271d5443",
+    "url": "/clicky-game/static/media/eyes10.5bb565e5.png"
+  },
+  {
+    "revision": "c3184a7cc3645e0b198717271d6afd3b",
+    "url": "/clicky-game/static/media/eyes11.c3184a7c.png"
+  },
+  {
+    "revision": "6ee41fb00ad6e86b15c79b6afbaab66e",
+    "url": "/clicky-game/static/media/eyes12.6ee41fb0.png"
+  },
+  {
+    "revision": "20b5ec399f55065f723cefafb6c360d8",
+    "url": "/clicky-game/static/media/eyes2.20b5ec39.png"
+  },
+  {
+    "revision": "a1bdaf35e11b22ae4d77b028bc9dfe81",
+    "url": "/clicky-game/static/media/eyes3.a1bdaf35.png"
+  },
+  {
+    "revision": "ed73b6582c1c4737e616f9c1b771c79f",
+    "url": "/clicky-game/static/media/eyes4.ed73b658.png"
+  },
+  {
+    "revision": "94dcff864af04f005ba5580622bdbbd4",
+    "url": "/clicky-game/static/media/eyes5.94dcff86.png"
+  },
+  {
+    "revision": "f97ed937cffd0ada0635fc737b1d1255",
+    "url": "/clicky-game/static/media/eyes6.f97ed937.png"
+  },
+  {
+    "revision": "4a54a1c6a507890503a48d201fcc72ca",
+    "url": "/clicky-game/static/media/eyes7.4a54a1c6.png"
+  },
+  {
+    "revision": "24b55d732dd861a2da076fe552ff75c9",
+    "url": "/clicky-game/static/media/eyes8.24b55d73.png"
+  },
+  {
+    "revision": "f52d091f4b127e76039ef6e25f723eab",
+    "url": "/clicky-game/static/media/eyes9.f52d091f.png"
+  }
+]);
