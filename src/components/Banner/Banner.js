72dd3bbdb17e957a454bb7ff5b8c42ef3fb03de7
chinedu moses <chinedu2moses@gmail.com>
2020-06-24 11:12:45 -0500

    banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => (
+  <div className="banner text-center d-flex align-items-center">
+    <p className="m-0">
+      Click on an image once to earn points. Click on image twice to lose all
+      your points! How good is your memory?
+    </p>
+  </div>
+);
+
+export default Banner;
