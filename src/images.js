5c8817f33033fff00fa23da1db9ed6ef0138f03c
chinedu moses <chinedu2moses@gmail.com>
2020-06-24 10:54:21 -0500

    images

---
+++ src/images.js
@@ -0,0 +1,29 @@
+import eyes1 from "./images/eyes1.png";
+import eyes2 from "./images/eyes2.png";
+import eyes3 from "./images/eyes3.png";
+import eyes4 from "./images/eyes4.png";
+import eyes5 from "./images/eyes5.png";
+import eyes6 from "./images/eyes6.png";
+import eyes7 from "./images/eyes7.png";
+import eyes8 from "./images/eyes8.png";
+import eyes9 from "./images/eyes9.png";
+import eyes10 from "./images/eyes10.png";
+import eyes11 from "./images/eyes11.png";
+import eyes12 from "./images/eyes12.png";
+
+const images = [
+  eyes1,
+  eyes2,
+  eyes3,
+  eyes4,
+  eyes5,
+  eyes6,
+  eyes7,
+  eyes8,
+  eyes9,
+  eyes10,
+  eyes11,
+  eyes12
+];
+
+export default images;
